24d53a6a77e1f260416264770642c383a49f79d8
teejaymoo <teajay.m88@gmail.com>
2021-04-19 08:08:37 -0500

    Feat: added delete and update

---
+++ curl-scripts/comment/update.sh
@@ -0,0 +1,15 @@
+API="http://localhost:4741"
+URL_PATH="/queries"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "comment": {
+      "response": "'"${RESPONSE}"'"
+    }
+  }'
+
+echo
