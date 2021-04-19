59023ce5c0ab217f9fc8d036aabf727455f2f230
teejaymoo <teajay.m88@gmail.com>
2021-04-19 08:16:30 -0500

    Feat: took username off of user schema

--- app/models/user.js
+++ app/models/user.js
@@ -6,11 +6,6 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
   hashedPassword: {
     type: String,
     required: true
